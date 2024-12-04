98dbeae6d1a2f20ce6a5ff718bd6181d75e9cd18
geraldaddey <geraldaddey@yahoo.com>
2024-12-04 16:50:03 +0000

    handle gestures on screens

--- src/navigations/Navigator.tsx
+++ src/navigations/Navigator.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { SplashScreen } from '../screens/Splash/SplashScreen';
 import { OnboardingScreen } from '../screens/Onboarding/OnboardingScreen';
 import { TabNavigator } from './TabNavigator';
-
+import { ProfileScreen } from '../screens/Tabs/ProfileScreen';
 import { RootStackParamList } from './Navigator.types';
 
 export const Navigator = () => {
@@ -15,11 +15,17 @@ export const Navigator = () => {
     <NavigationContainer>
       <Stack.Navigator 
         initialRouteName="Splash"
-        screenOptions={{ headerShown: false }}
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+          animationEnabled: true,
+        }}
       >
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Onboarding" component={OnboardingScreen} />
         <Stack.Screen name="MainTabs" component={TabNavigator} />
+        <Stack.Screen name="Profile" component={ProfileScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
